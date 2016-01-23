1babf5181fdb22c38f44f961dcedb6e4fe5747ca
makhmudislamov <makhmudsunnatovich@Johns-MBP.att.net>
2016-01-22 23:19:07 -0800

    images are added

--- Flicks/MoviesViewController.swift
+++ Flicks/MoviesViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AFNetworking
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
@@ -82,7 +83,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
         let imageUrl = NSURL(string: baseUrl + posterPath)
         
-        cell.posterView = setImage
+        cell.posterView.setImageWithURL(imageUrl!)
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
         
